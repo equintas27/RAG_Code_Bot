837bbdff9e05a1121eb49378254b5c653f048f33
equintas27 <eugenioquintas20@gmail.com>
2026-08-28 19:58:47 +0100

    Refactoring text_splitter

--- src/codebot/core/chunking/text_splitter.py
+++ src/codebot/core/chunking/text_splitter.py
@@ -1,25 +1,31 @@
 def text_splitter(text, chunk_size, overlap) -> list[str]:
     
-    chunks = []
-    start = 0
+    #chunks = []
+    #start = 0
 
-    if chunk_size <= 0 or overlap <= 0:
-        raise ValueError("chunk_size and overlap must be biggest than 0")
-    if overlap >= chunk_size:
-        raise ValueError("Overlap must be smaller than chunk_size")
+    #if chunk_size <= 0 or overlap <= 0:
+        #raise ValueError("chunk_size and overlap must be biggest than 0")
+    #if overlap >= chunk_size:
+       # raise ValueError("Overlap must be smaller than chunk_size")
    
-    while start < len(text):
-        end = start + chunk_size
-        chunk = text[start:end]
+   # while start < len(text):
+       # end = start + chunk_size
+        #chunk = text[start:end]
 
-        chunks.append(chunk)
-        start = start + chunk_size - overlap
-    return chunks 
+        #chunks.append(chunk)
+        #start = start + chunk_size - overlap
+    #return chunks 
+    paragraphs = text.split("\n\n")
+    print(paragraphs[:5])
 
 if __name__ == "__main__":
-    text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chunk_size = 10
-    overlap = 3
+  text = """Primeira paragem.
 
-    s = text_splitter(text, chunk_size, 3)
-    print (s)
+    Segunda paragem.
+
+    Terceira paragem."""
+    #chunk_size = 10
+    #overlap = 3
+    text_splitter(text, 10, 3)
+   # s = text_splitter(text, chunk_size, 3)
+    #print (s)
--- src/codebot/core/indexing/ingest.py
+++ src/codebot/core/indexing/ingest.py
@@ -1,15 +1,20 @@
 import pymupdf
+from codebot.core.chunking.text_splitter import text_splitter
 
-pdf_path = "data/documents/sonangol/Relatorio-2025.pdf"
+def extract_pdf(pdf_path) ->str:    
+    document = pymupdf.open(pdf_path)
 
-document = pymupdf.open(pdf_path)
+    full_text = ""
 
-print(f"Número de páginas: {len(document)}")
-full_text = ""
+    for page in document:
+        text = page.get_text()
+        full_text += text
+    return (full_text)
 
-for page in document:
-    text = page.get_text()
-    full_text += text
 
-print ("\n --- Páginas --- \n")
-print(full_text)
+if __name__ == "__main__":
+    path = "data/documents/sonangol/Relatorio-2025.pdf"
+    text_1 = extract_pdf(path)
+    txt = text_splitter(text_1, 1000, 200)
+    print ("\n --- Páginas --- \n")
+    print(txt[0])
