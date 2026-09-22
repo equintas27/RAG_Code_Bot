471bef72bfb8931276840659603674e4e8d63cd0
equintas27 <eugenioquintas20@gmail.com>
2026-09-22 18:07:38 +0100

    Finishing embeddings functions

--- src/codebot/__main__.py
+++ src/codebot/__main__.py
@@ -1,13 +1,12 @@
 from tests.aux_functions_tests import find_overlap, reconstruct_chunks
 from codebot.core.chunking.text_splitter import text_splitter
 from codebot.core.chunking.pdf_extractor import extract_pdf_with_metadata
-from codebot.core.embeddings.embedder import generate_chunk_embedding
+from codebot.core.embeddings.embedder import generate_chunk_embedding, adding_embedding
 from codebot.core.embeddings.embeddingmodel import EmbeddingModel
 
 if __name__ == "__main__":
     path = "data/documents/sonangol/Relatorio-2025.pdf"
     chunks = extract_pdf_with_metadata(path)
 
-    embedding_model = EmbeddingModel ()
-    generate_chunk_embedding (chunks[0]["content"], embedding_model)
+    new_chunks = adding_embedding(chunks)
     
--- src/codebot/core/embeddings/embedder.py
+++ src/codebot/core/embeddings/embedder.py
@@ -1,8 +1,17 @@
 from .embeddingmodel import EmbeddingModel
+import numpy
 
-def generate_chunk_embedding(chunk: str, embedding_model: EmbeddingModel) :
-
+def generate_chunk_embedding(chunk: str, embedding_model: EmbeddingModel) ->  list[numpy.ndarray]:
     vetor = embedding_model.model.encode_document(chunk)
-    print(type(vetor))
-    print(len(vetor))
-    print(vetor)
+    return (vetor)
+
+def adding_embedding(chunks: list[dict]) -> list[dict]:
+    
+    embedding_model = EmbeddingModel ()
+    
+    for chunk in chunks:
+        print(f"ID da chunk: {chunk["metadata"]["id"]}")
+        emb = generate_chunk_embedding(chunk["content"], embedding_model)
+        chunk["embedding"] = emb
+        print(f"Tamanho do embedding {len(chunk["embedding"])}")
+    return (chunks)
