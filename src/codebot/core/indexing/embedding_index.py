43c84eb2d0684c5fe21dabc5efc23ec877eb9e8f
equintas27 <eugenioquintas20@gmail.com>
2026-09-23 16:50:48 +0100

    starting indexing vetorial

---
+++ src/codebot/core/indexing/embedding_index.py
@@ -0,0 +1,11 @@
+import numpy
+
+class EmbeddingIndex:
+    def __init__(self):
+        self._embeddings = []
+        self._chunk_ids = []
+
+    def add(embedding: list[numpy.ndarray], ):
+
+    def search():
+
